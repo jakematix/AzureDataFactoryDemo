185223b4c94d3b3505ebefbdd756a38db5124195
jakematix <jarmo.kant@gmail.com>
2024-02-05 09:59:35 +0200

    New varia1bles added

--- SqlDatabase/variables.tf
+++ SqlDatabase/variables.tf
@@ -1,28 +1,33 @@
 variable "name_construct" {
-    type        = string
-    description = "The basic name of to construct the actual name"
+  type        = string
+  description = "The basic name of to construct the actual name"
 }
 
 variable "rg_name" {
-    type        = string
-    description = "The RG name"
+  type        = string
+  description = "The RG name"
 }
 
 variable "sql_db_username" {
-    type        = string
-    description = "SQL Database username"
-    sensitive   = true
-}
-
-variable "sql_db_password" {
-    type        = string
-    description = "SQL Database password"
-    sensitive   = true
+  type        = string
+  description = "SQL Database username"
+  sensitive   = true
 }
 
 variable "region" {
-    type = string
-    description = "Azure region name"
+  type        = string
+  description = "Azure region name"
 }
 
+variable "allowed_ip" {
+  type        = string
+  description = "Allowed IP address for SQL login"
+
+}
+
+variable "az_keyvalt_id" {
+  type        = string
+  description = "Azure Key Vault id to store the SQL database password"
+
+}
 
