303918214ef83e3581c9101ab3e410e44ffa0dae
jakematix <jarmo.kant@gmail.com>
2024-02-05 09:57:45 +0200

    New outputs added

--- ResourceGroup/outputs.tf
+++ ResourceGroup/outputs.tf
@@ -1,3 +1,11 @@
 output "rg_out_name" {
-    value = azurerm_resource_group.rg.name
+  value = azurerm_resource_group.rg.name
+}
+
+output "rg_id" {
+  value = azurerm_resource_group.rg.id
+}
+
+output "kv_secrets_id" {
+  value = azuread_group.kv_secrets_officers_group.id
 }
