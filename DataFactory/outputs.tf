b94223e4c04a877bed85b6ab0df4cdb5895532ca
jakematix <jarmo.kant@gmail.com>
2024-02-05 12:10:33 +0200

    Fixed things

--- DataFactory/outputs.tf
+++ DataFactory/outputs.tf
@@ -1,3 +1,4 @@
-output "DataFactoryId" {
-  value = azurerm_data_factory.datafactory.identity.0.principal_id
+output "data_factory_id" {
+  value       = azurerm_data_factory.datafactory.identity.0.principal_id
+  description = "Data Factory Id"
 }
