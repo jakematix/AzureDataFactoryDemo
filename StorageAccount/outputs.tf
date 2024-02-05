bd36a641d9016b21cd4082556c3c97f2b65a8d03
jakematix <jarmo.kant@gmail.com>
2024-02-05 10:00:53 +0200

    New outputs added

--- StorageAccount/outputs.tf
+++ StorageAccount/outputs.tf
@@ -8,9 +8,13 @@ output "containername_out" {
 
 output "blob_file_out" {
   value = resource.azurerm_storage_blob.blobupload.name
-  
+
 }
 
 output "primary_blob_connection_string_out" {
   value = resource.azurerm_storage_account.StgAccount.primary_blob_connection_string
+}
+
+output "connectionstring_kv_secret_name" {
+  value = resource.azurerm_key_vault_secret.blob_secret.name
 }
