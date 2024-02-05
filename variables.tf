fb5d781ee8ae781f9868577b3f363f10227faa41
jakematix <jarmo.kant@gmail.com>
2024-02-05 09:53:17 +0200

    New variable added

--- variables.tf
+++ variables.tf
@@ -1,11 +1,48 @@
 variable "sql_db_username" {
-    type        = string
-    description = "SQL Database username"
-    sensitive   = true
+  type        = string
+  description = "SQL Database username"
+  sensitive   = true
 }
 
-variable "sql_db_password" {
-    type        = string
-    description = "SQL Database password"
-    sensitive   = true
+
+variable "allowed_sql_login_ip" {
+  type        = string
+  description = "Allowed login IP address for SQL server login"
+  #    sensitive   = true
+}
+
+variable "demo_sub_id" {
+  type        = string
+  description = "Subscription ID is stored in $Env:TF_VAR_demo_sub_id"
+}
+variable "demo_client_id" {
+  type        = string
+  description = "Client ID is stored in $Env:TF_VAR_demo_client_id"
+}
+
+variable "demo_object_id" {
+  type        = string
+  description = "Object ID is stored in $Env:TF_VAR_demo_object_id"
+}
+
+variable "demo_tenant_id" {
+  type        = string
+  description = "Tenant ID is stored in $Env:TF_VAR_demo_tenant_id"
+}
+
+variable "demo_client_secret" {
+  type        = string
+  description = "Client Secret is stored in $Env:TF_VAR_demo_client_secret"
+  sensitive   = true
+}
+
+# Variable env_name is used as name contstructor in the demo
+variable "env_name" {
+  type        = string
+  description = "Environment name that is used as the prefix in the resource names"
+}
+
+variable "azure_region" {
+  type        = string
+  description = "Desired Azure Region"
 }
