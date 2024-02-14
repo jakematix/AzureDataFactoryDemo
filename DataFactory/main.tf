e6fb29b8429f4d11797fa47e5f41785a3867a8f3
jakematix <jarmo.kant@gmail.com>
2024-02-14 16:11:43 +0200

    Creates Managed Private Endpoints and new Integration Runtime

--- DataFactory/main.tf
+++ DataFactory/main.tf
@@ -7,14 +7,15 @@ resource "random_string" "rnd" {
   upper   = false
 }
 
-# Creation of Azure Data Factory
+# Creation of Azure Data Factory with Managed Virtual Network 
 resource "azurerm_data_factory" "datafactory" {
-  name                = "${var.name_construct}${random_string.rnd.result}"
-  location            = var.region
-  resource_group_name = var.rg_name
+  name                            = "${var.name_construct}${random_string.rnd.result}"
+  location                        = var.region
+  resource_group_name             = var.rg_name
   identity {
     type = "SystemAssigned"
   }
+  managed_virtual_network_enabled = true
 }
 
 # Role assignment for the Data Factory as Key Vault Secrets Officer in order to get secrets from the Key Vault
@@ -42,40 +43,75 @@ data "azurerm_key_vault_secret" "sql_conn_string" {
   key_vault_id = var.kv_id
 }
 
+# Creation of the new Integration Runtime
+resource "azurerm_data_factory_integration_runtime_azure" "ir" {
+  name                    = "${var.name_construct}-IR"
+  data_factory_id         = azurerm_data_factory.datafactory.id
+  location                = var.region
+  virtual_network_enabled = true
+  compute_type            = "General"
+  core_count              = 8
+  time_to_live_min        = 10
+}
+
+# Creation of the Managed Private Endpoint to Blob storage.
+# NOTE! You need to manually approve the Endpoint in Azure Portal as post work
+resource "azurerm_data_factory_managed_private_endpoint" "df_managed_endpoint_storage" {
+  name               = "${var.name_construct}-AzureBlob-MPE"
+  data_factory_id    = azurerm_data_factory.datafactory.id
+  target_resource_id = var.storage_account_id
+  subresource_name   = "blob"
+}
+
+
 # Creation of the Linked service to the Blob
+#  connection_string       = data.azurerm_key_vault_secret.storage_sec.value
 resource "azurerm_data_factory_linked_service_azure_blob_storage" "bloblink" {
-  name              = "${var.name_construct}BlobLink"
-  data_factory_id   = azurerm_data_factory.datafactory.id
-  connection_string = data.azurerm_key_vault_secret.storage_sec.value
+  name                     = "${var.name_construct}BlobLink"
+  data_factory_id          = azurerm_data_factory.datafactory.id
+  connection_string        = data.azurerm_key_vault_secret.storage_sec.value
+  integration_runtime_name = azurerm_data_factory_integration_runtime_azure.ir.name
 }
 
 # Settings for delimited text aka. CSV file
-resource "azurerm_data_factory_dataset_delimited_text" "csvfile" {
-  name                = "${var.name_construct}csv"
-  data_factory_id     = azurerm_data_factory.datafactory.id
-  linked_service_name = azurerm_data_factory_linked_service_azure_blob_storage.bloblink.name
+#resource "azurerm_data_factory_dataset_delimited_text" "csvfile" {
+#  name                = "${var.name_construct}csv"
+#  data_factory_id     = azurerm_data_factory.datafactory.id
+#  linked_service_name = azurerm_data_factory_linked_service_azure_blob_storage.bloblink.name
+#
+#  azure_blob_storage_location {
+#    filename  = "GlobalLandTemperatures.csv"
+#    container = "sourcedata"
+#  }
+#
+#  column_delimiter    = ","
+#  quote_character     = "\""
+#  escape_character    = "\\"
+#  first_row_as_header = true
+#}
 
-  azure_blob_storage_location {
-    filename  = "GlobalLandTemperatures.csv"
-    container = "sourcedata"
-  }
-
-  column_delimiter    = ","
-  quote_character     = "\""
-  escape_character    = "\\"
-  first_row_as_header = true
+# Creation of the Managed Private Endpoint to SQL Server.
+# NOTE! You need to manually approve the Endpoint in Azure Portal as post work
+resource "azurerm_data_factory_managed_private_endpoint" "df_managed_endpoint_sql_server" {
+  name               = "${var.name_construct}-AzureSQL-MPE"
+  data_factory_id    = azurerm_data_factory.datafactory.id
+  target_resource_id = var.sql_server_id
+  subresource_name   = "sqlServer"
 }
 
+
 # Create link to SQL Database
+# connection_string = data.azurerm_key_vault_secret.sql_conn_string.value
 resource "azurerm_data_factory_linked_service_azure_sql_database" "sqllink" {
-  name              = "${var.name_construct}SQLLink"
-  data_factory_id   = azurerm_data_factory.datafactory.id
-  connection_string = data.azurerm_key_vault_secret.sql_conn_string.value
+  name                      = "${var.name_construct}SQLLink"
+  data_factory_id           = azurerm_data_factory.datafactory.id
+  integration_runtime_name  = azurerm_data_factory_integration_runtime_azure.ir.name
+  connection_string         = data.azurerm_key_vault_secret.sql_conn_string.value
 }
 
 # Create a dataset to SQL table (note that the table does not exist. Need to be create using SQL before executing the demo)
-resource "azurerm_data_factory_dataset_azure_sql_table" "sql_dataset" {
-  name                = "${var.name_construct}sql"
-  data_factory_id     = azurerm_data_factory.datafactory.id
-  linked_service_id   = azurerm_data_factory_linked_service_azure_sql_database.sqllink.id
-}
+#resource "azurerm_data_factory_dataset_azure_sql_table" "sql_dataset" {
+#  name                = "${var.name_construct}sql"
+#  data_factory_id     = azurerm_data_factory.datafactory.id
+#  linked_service_id   = azurerm_data_factory_linked_service_azure_sql_database.sqllink.id
+#}
