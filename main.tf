5318f9127bb9602b907da59058917d70d3717593
jakematix <jarmo.kant@gmail.com>
2024-02-14 16:19:38 +0200

    Many changes related to the new setup

--- main.tf
+++ main.tf
@@ -7,6 +7,18 @@ module "ResourceGroup" {
   region         = var.azure_region
 }
 
+# Creation of the Network Security Group, Virtual Network (VNet) and the subnet
+module "VirtualNetwork" {
+  source                    = "./VirtualNetwork"
+  vnet_name                 = "VNet"
+  subnet_name               = "FrontEnd"
+  region                    = var.azure_region
+  rg_name                   = module.ResourceGroup.rg_out_name
+  vnet_address_space        = "10.0.0.0/16"
+  vnet_subnet_address_space = "10.0.0.0/24"
+  allowed_ip_address        = var.allowed_ip_address
+}
+
 # Next, Create Azure Key Vault and access groups
 module "KeyVault" {
   source         = "./KeyVault"
@@ -15,29 +27,42 @@ module "KeyVault" {
   rg_name        = module.ResourceGroup.rg_out_name
 }
 
-# Next, the Storage Account with Blob and the source data is uploaded.
+# Creation of the Front End VM
+# * Size: Standard B1s (1 vcpu, 1 GiB memory)
+# VM Admin username must be present in environment variable $Env:TF_VAR_vm_admin_username
+# VM Admin password must be present in environment variable $Env:TF_VAR_vm_admin_password
+module "VirtualMachine" {
+  source            = "./VirtualMachine"
+  vm_name           = "FrontEndVM"
+  region            = var.azure_region
+  rg_name           = module.ResourceGroup.rg_out_name
+  vm_size           = "Standard_B1s"
+  vm_admin_username = var.vm_admin_username
+  vm_admin_password = var.vm_admin_password
+  subnet_id         = module.VirtualNetwork.subnet_id
+}
+
+# Creation of the Storage Account
 module "StorageAccount" {
   source         = "./StorageAccount"
   name_construct = var.env_name
   region         = var.azure_region
   rg_name        = module.ResourceGroup.rg_out_name
-  container_name = "sourcedata"
-  blobname       = "GlobalLandTemperatures.csv"
-  sourcepath     = "./data/GlobalLandTemperatures.csv"
+  vnet_id        = module.VirtualNetwork.vnet_id
+  subnet_id      = module.VirtualNetwork.subnet_id
   az_keyvault_id = module.KeyVault.azure_kv_id
 }
 
 # Ensure, that SQL Database username is in environment variable $env:TF_VAR_sql_db_username.
-# In order to grant access to the SQL Server from the home IP, the $env:TF_VAR_home_ip need to be defined 
-# with your home IP address.
 module "SqlDatabase" {
   source          = "./SqlDatabase"
   name_construct  = var.env_name
   region          = var.azure_region
   rg_name         = module.ResourceGroup.rg_out_name
   sql_db_username = var.sql_db_username
+  vnet_id         = module.VirtualNetwork.vnet_id
+  subnet_id       = module.VirtualNetwork.subnet_id
   az_keyvalt_id   = module.KeyVault.azure_kv_id
-  allowed_ip      = var.allowed_sql_login_ip
 }
 
 module "DataFactory" {
@@ -50,6 +75,9 @@ module "DataFactory" {
   kv_secrets_officer_group_id                 = module.ResourceGroup.kv_secrets_id
   storage_kv_secret_name                      = module.StorageAccount.connectionstring_kv_secret_name
   sql_server_connection_string_kv_secret_name = module.SqlDatabase.sql_connection_string_kv_secret_name
+  sql_server_id                               = module.SqlDatabase.sql_server_id
+  storage_account_id                          = module.StorageAccount.storage_account_id
+  
 
 }
 
