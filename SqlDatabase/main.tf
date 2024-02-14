6a74ff4c92a5cd70ea21ee2e931d275c6a4557d1
jakematix <jarmo.kant@gmail.com>
2024-02-14 16:15:15 +0200

    Creates Private Endpoints

--- SqlDatabase/main.tf
+++ SqlDatabase/main.tf
@@ -18,6 +18,25 @@ resource "azurerm_key_vault_secret" "sql_password" {
   value        = "${random_id.rnd.hex}SQ51"
   key_vault_id = var.az_keyvalt_id
 }
+
+# Create a Private DNS Zone for private endpoint to Blob
+resource "azurerm_private_dns_zone" "priv_dns_zone" {
+  name                = "privatelink.database.windows.net"
+  resource_group_name = var.rg_name
+}
+
+
+# Create Virtual Network link for the zone
+resource "azurerm_private_dns_zone_virtual_network_link" "dns_zone_vlink" {
+  name                  = "dns-link"
+  resource_group_name   = var.rg_name
+  private_dns_zone_name = azurerm_private_dns_zone.priv_dns_zone.name
+  virtual_network_id    = var.vnet_id
+  depends_on            = [ azurerm_private_dns_zone.priv_dns_zone ]
+}
+
+
+
 # Creates MS SQL Database and SQL Server
 # SA authentication is used, username and password are provided as environment variables
 
@@ -28,13 +47,13 @@ resource "azurerm_mssql_server" "sqlserver" {
   version                       = "12.0"
   administrator_login           = var.sql_db_username
   administrator_login_password  = azurerm_key_vault_secret.sql_password.value
-  public_network_access_enabled = true
+  public_network_access_enabled = false
 }
 
 resource "azurerm_mssql_database" "sqldatabase" {
-  name      = "${var.name_construct}SQL"
-  server_id = azurerm_mssql_server.sqlserver.id
-  collation = "SQL_Latin1_General_CP1_CI_AS"
+  name                         = "${var.name_construct}SQL"
+  server_id                    = azurerm_mssql_server.sqlserver.id
+  collation                    = "SQL_Latin1_General_CP1_CI_AS"
 
   auto_pause_delay_in_minutes = 60
   max_size_gb                 = 1
@@ -53,20 +72,24 @@ resource "azurerm_key_vault_secret" "sql_connection_string" {
 }
 
 
-# The Azure feature Allow access to Azure services 
-# can be enabled by setting start_ip_address and end_ip_address to 0.0.0.0
-# which (is documented in the Azure API Docs).
-resource "azurerm_mssql_firewall_rule" "firewallrules" {
-  name             = "AllowAzureServices"
-  server_id        = azurerm_mssql_server.sqlserver.id
-  start_ip_address = "0.0.0.0"
-  end_ip_address   = "0.0.0.0"
-}
 
-resource "azurerm_mssql_firewall_rule" "homerule" {
-  name             = "AllowHomeConnection"
-  server_id        = azurerm_mssql_server.sqlserver.id
-  start_ip_address = var.allowed_ip
-  end_ip_address   = var.allowed_ip
-}
+# Creation of the Private Endpoint in the given subnet
+resource "azurerm_private_endpoint" "vm_sql_private_endpoint" {
+    name                             = "VM-SQL-Private-Endpoint"
+    location                         = var.region
+    resource_group_name              = var.rg_name
+    subnet_id                        = var.subnet_id
+    
+    private_dns_zone_group {
+      name                           = "default"
+      private_dns_zone_ids           = [azurerm_private_dns_zone.priv_dns_zone.id]
+    }
 
+    private_service_connection {
+      name                           = "vmssql-private-service-connection"
+      private_connection_resource_id = azurerm_mssql_server.sqlserver.id
+      subresource_names              = ["SqlServer"]
+      is_manual_connection           = false
+    }
+    depends_on                       = [ azurerm_mssql_database.sqldatabase ]
+}
