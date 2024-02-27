9b28b1b8ac969d1844f6b92fa00b1ed8de965506
jakematix <jarmo.kant@gmail.com>
2024-02-27 11:57:00 +0200

    Some outputs added and some removed

--- outputs.tf
+++ outputs.tf
@@ -14,6 +14,11 @@ output "sql_server_name" {
   description = "SQL Server Name"
 }
 
+output "keyvault_name" {
+  value       = module.KeyVault.azure_kv_name
+  description = "Azure Key Vault Name"
+
+}
 output "sql_secret_key_pwd" {
   value       = module.SqlDatabase.sql_server_password_kv_secret_name
   description = "Name of the secret key in the Key Vault that refers to SQL server password"
@@ -27,4 +32,10 @@ output "sql_secret_key_connectionstring" {
 output "dns_inbound_endpoint" {
   value       = module.PrivateDNSResolver.dns_inbound_endpoint
   description = "DNS Inbound IP"
+}
+
+output "storage_secret_key_connectionstring" {
+  value       = module.StorageAccount.connectionstring_kv_secret_name
+  description = "Name of the secret key in the Key Vault that refers to storage connection string"
+
 }
