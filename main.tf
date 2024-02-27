41251e7b2c209202caf3a84ed216e7065a252cfe
jakematix <jarmo.kant@gmail.com>
2024-02-27 11:56:35 +0200

    Changes to DataFactory and KeyVault changes

--- main.tf
+++ main.tf
@@ -8,6 +8,7 @@ module "ResourceGroup" {
 }
 
 # Creation of the Network Security Group, Virtual Network (VNet) and the subnet
+# allowed_ip_address        = var.allowed_ip_address
 module "VirtualNetwork" {
   source                    = "./VirtualNetwork"
   vnet_name                 = "VNet"
@@ -16,36 +17,38 @@ module "VirtualNetwork" {
   rg_name                   = module.ResourceGroup.rg_out_name
   vnet_address_space        = "10.0.0.0/16"
   vnet_subnet_address_space = "10.0.0.0/24"
-  allowed_ip_address        = var.allowed_ip_address
+
 }
 
 # Creation of the Virtual Network Gateway (point-to-site) and Gateway Subnet in the Virtual Network.
 # Note that Gatewaty creation can take up to 60 minutes
 module "VirtualNetworkGateway" {
-    source                            = "./VirtualNetworkGateway"
-    region                            = var.azure_region
-    rg_name                           = module.ResourceGroup.rg_out_name
-    vnet_name                         = module.VirtualNetwork.vnet_name
-    vnet_gateway_subnet_address_space = "10.0.1.0/27"
-    p2s_address_pool                  = "172.16.201.0/24"
+  source                            = "./VirtualNetworkGateway"
+  region                            = var.azure_region
+  rg_name                           = module.ResourceGroup.rg_out_name
+  vnet_name                         = module.VirtualNetwork.vnet_name
+  vnet_gateway_subnet_address_space = "10.0.1.0/27"
+  p2s_address_pool                  = "172.16.201.0/24"
 }
 
 # Creation of the Private DNS Resolver with Inboound Endpoint
 module "PrivateDNSResolver" {
-    source                   = "./PrivateDNSResolver"
-    region                   = var.azure_region
-    rg_name                  = module.ResourceGroup.rg_out_name
-    vnet_id                  = module.VirtualNetwork.vnet_id
-    vnet_name                = module.VirtualNetwork.vnet_name
-    inb_subnet_address_space = "10.0.1.32/28" 
+  source                   = "./PrivateDNSResolver"
+  region                   = var.azure_region
+  rg_name                  = module.ResourceGroup.rg_out_name
+  vnet_id                  = module.VirtualNetwork.vnet_id
+  vnet_name                = module.VirtualNetwork.vnet_name
+  inb_subnet_address_space = "10.0.1.32/28"
 }
 
 # Next, Create Azure Key Vault and access groups
+# Allow access only from allowed IP address
 module "KeyVault" {
-  source         = "./KeyVault"
-  name_construct = var.env_name
-  region         = var.azure_region
-  rg_name        = module.ResourceGroup.rg_out_name
+  source             = "./KeyVault"
+  name_construct     = var.env_name
+  region             = var.azure_region
+  rg_name            = module.ResourceGroup.rg_out_name
+  allowed_ip_address = var.allowed_ip_address
 }
 
 # Creation of the Storage Account
@@ -83,6 +86,8 @@ module "DataFactory" {
   sql_server_connection_string_kv_secret_name = module.SqlDatabase.sql_connection_string_kv_secret_name
   sql_server_id                               = module.SqlDatabase.sql_server_id
   storage_account_id                          = module.StorageAccount.storage_account_id
+  vnet_id                                     = module.VirtualNetwork.vnet_id
+  subnet_id                                   = module.VirtualNetwork.subnet_id
 }
 
 
